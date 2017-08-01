d30f720a8333ea9d0229c23e3b1dc354edeca967
Eddy <Eduardo Garcia-Montes>
2017-08-01 13:51:14 -0400

    add initial commit

---
+++ pozyx_receiver_range.ino/pozyx_receiver_range.ino.ino
@@ -0,0 +1,68 @@
+#include <Pozyx.h>
+#include <Pozyx_definitions.h>
+#include <Wire.h>
+#include <ros.h>
+#include <sensor_msgs/Range.h>
+
+ros::NodeHandle  nh;
+sensor_msgs::Range range_msg;
+ros::Publisher pub_range( "range_data", &range_msg);
+
+const char *car_ids[4] = {"6802","6835","6806","6867"}; //every car id
+const char *car_num[4] = {"car0","car0","car1","car1"}; //assign car number to each id
+int repeat[] = {0,1};
+int num_ids = sizeof(car_ids)/2; //amount of total cars
+String my_id;
+
+  
+void setup(){
+  Serial.begin(57600);
+  nh.initNode();
+  nh.advertise(pub_range);
+  // initialize Pozyx
+  if(! Pozyx.begin(false, MODE_INTERRUPT, POZYX_INT_MASK_RX_DATA, 0)){
+    Serial.println("ERROR: Unable to connect to POZYX shield");
+    Serial.println("Reset required");
+    abort();
+  }
+}
+
+void loop(){
+  print_message();
+}
+
+void print_message() { 
+  if(Pozyx.waitForFlag(POZYX_INT_STATUS_RX_DATA,100)){ //wait up to 50ms for a message
+    // we have received a message!
+    // who sent the message?
+    uint16_t messenger = 0x00;
+    delay(1);
+    Pozyx.getLastNetworkId(&messenger);
+    char data[11];
+    String your_id = String(messenger,HEX); //sender id
+    // read the contents of the receive (RX) buffer, this is the msesage that was sent to this device
+    Pozyx.readRXBufferData((uint8_t *) data, 11); 
+    String my_id = String(data).substring(0,4); //sender id
+    String meters = String(data).substring(5); //distance
+    //Did we receive data, and is it reasonable?
+    if (sizeof(data) > 0 and meters.toFloat() < 100){ 
+      //What car are we?
+      for (int a = 0;a < num_ids; a++) {
+        if (my_id == car_ids[a]) { 
+          range_msg.header.frame_id = car_num[a];
+        }
+        //what car are you? 
+        else if (your_id == car_ids[a]) {
+          range_msg.field_of_view = String(car_num[a][3]).toFloat();
+        }
+      }
+      //Zero meters is a misread
+      if (meters.toFloat() != 0) { 
+        range_msg.range = meters.toFloat();
+        pub_range.publish(&range_msg);
+        nh.spinOnce();
+      }
+    }
+  }
+}
+
---
+++ pozyx_sender_range/pozyx_sender_range.ino
@@ -0,0 +1,67 @@
+/**
+  The pozyx chat demo
+  please check out https://www.pozyx.io/Documentation/Tutorials/getting_started
+
+  This demo requires at least two pozyx shields and an equal number of Arduino's.
+  It demonstrates the wireless messaging capabilities of the pozyx device.
+
+  This demo creates a chat room. Text written in the Serial monitor will be broadcasted to all other pozyx devices
+  within range. They will see your message appear in their Serial monitor.
+*/
+
+#include <Pozyx.h>
+#include <Pozyx_definitions.h>
+#include <Wire.h>
+
+uint16_t source_id;                 // the network id of this device
+uint16_t chat_id = 0;             //Broadcast the message
+String distance = "";            // a string to hold incoming data
+
+uint8_t ranging_protocol = POZYX_RANGE_PROTOCOL_PRECISION; // ranging protocol of the Pozyx.
+const int num_cars = 1;
+uint16_t car_ids[num_cars] = {0x6806}; //Default is car0 so only range car1
+
+void setup(){
+  Serial.begin(57600);
+  // initialize Pozyx
+  if(Pozyx.begin() == POZYX_FAILURE){
+    Serial.println("ERROR: Unable to connect to POZYX shield");
+    Serial.println("Reset required");
+    delay(100);
+    abort();
+  }
+  // read the network id of this device
+  Pozyx.regRead(POZYX_NETWORK_ID, (uint8_t*)&source_id, 2);
+  if (String(source_id,HEX) == "6867"){
+    car_ids[0] = 0x6802; //This is car1 so only range car0
+  }
+}
+
+void loop(){
+  send_message();
+}
+
+void send_message() {
+  for (int i = 0;i < num_cars;i++) { 
+    if (String(source_id,HEX) != String(car_ids[i],HEX)) {
+      device_range_t range;
+      int status = 0;
+      while (status != POZYX_SUCCESS){
+        status = Pozyx.doRanging(car_ids[i], &range);
+        if (status == POZYX_SUCCESS) {
+          distance = String(car_ids[i], HEX) + "," + String(range.distance/1000.0,3);
+          int length = String(distance).length()+1;
+          uint8_t buffer[length];
+          String(distance).getBytes(buffer, length);   
+          // write the message to the transmit (TX) buffer
+          status = Pozyx.writeTXBufferData(buffer, length);
+          // broadcast the contents of the TX buffer
+          status = Pozyx.sendTXBufferData(chat_id);
+          break;
+        }
+      }
+    }
+  }
+}
+
+
