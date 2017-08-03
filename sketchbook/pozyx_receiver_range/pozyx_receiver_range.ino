fff597165c69dfec096caf9da2a6aa7fdcf342fc
Alexander Wallar <alex@wallar.me>
2017-08-02 21:01:10 -0400

    moved everything to sketchbook. starting to test with dynamic ids

---
+++ sketchbook/pozyx_receiver_range/pozyx_receiver_range.ino
@@ -0,0 +1,123 @@
+#include <Pozyx.h>
+#include <Pozyx_definitions.h>
+#include <Wire.h>
+#include <ros.h>
+#include <sensor_msgs/Range.h>
+#include <multi_car_msgs/CarMeasurement.h>
+
+ros::NodeHandle  nh;
+/* sensor_msgs::Range range_msg; */
+multi_car_msgs::CarMeasurement meas;
+/* ros::Publisher pub_range( "range_data", &range_msg); */
+ros::Publisher pub_meas("measurements", &meas);
+
+const char *car_ids[6] = {"6802","6835","6806","6867","6827","685b"}; //every car id (Receiver, Sender)
+const int num_ids = sizeof(car_ids)/2; //amount of total ids
+const char *car_num[num_ids] = {"car0","car0","car1","car1","car2","car2"}; //assign car number to each id
+int repeat[] = {0,1};
+String my_id;
+
+void setup_uwb()
+{
+    UWB_settings_t uwb_settings;
+    Pozyx.getUWBSettings(&uwb_settings);
+    uwb_settings.bitrate = 2;
+    uwb_settings.plen = 0x24;
+    Pozyx.setUWBSettings(&uwb_settings);
+}
+
+void setup(){
+  Serial.begin(57600);
+  nh.initNode();
+  nh.advertise(pub_meas);
+  // initialize Pozyx
+  if(! Pozyx.begin(false, MODE_INTERRUPT, POZYX_INT_MASK_RX_DATA, 0)){
+    Serial.println("ERROR: Unable to connect to POZYX shield");
+    Serial.println("Reset required");
+    abort();
+  }
+  Pozyx.setOperationMode(POZYX_TAG_MODE);
+  //setup_uwb();
+  //Serial.flush();
+}
+
+void loop(){
+  print_message();
+}
+
+typedef struct dq_range_t {
+    uint16_t id;
+    float dist;
+} dq_range;
+
+typedef struct dq_header_t {
+    uint8_t sensor_type;
+    uint16_t num_data;
+} dq_header;
+
+void parse_data(uint8_t *data)
+{
+    dq_header header;
+    memcpy(&header, data, sizeof(dq_header));
+
+    uint8_t *raw = data + sizeof(dq_header);
+
+    // if range data
+    if (header.sensor_type == 0)
+    {
+        dq_range rng;
+        for (int i = 0; i < header.num_data; i++)
+        {
+            memcpy(&rng, raw, sizeof(dq_range));
+            /* meas.header.frame_id = rng.id; */
+            String(rng.id, HEX).toCharArray(meas.header.frame_id, sizeof(uint16_t));
+            meas.control.steering_angle = rng.dist;
+            pub_meas.publish(&meas);
+        }
+    }
+}
+
+void print_message() {
+  if(Pozyx.waitForFlag(POZYX_INT_STATUS_RX_DATA,100)){ //wait up to 50ms for a message
+    // we have received a message!
+    // who sent the message?
+    uint16_t messenger = 0x00;
+    uint8_t length = 0;
+    delay(1);
+    Pozyx.getLastNetworkId(&messenger);
+    Pozyx.getLastDataLength(&length);
+    /* char data[length]; */
+    String your_id = String(messenger,HEX); //sender id
+    // read the contents of the receive (RX) buffer, this is the msesage that was sent to this device
+
+    uint8_t data[length];
+    Pozyx.readRXBufferData(data, length);
+    parse_data(data);
+    /* String my_id = String(data).substring(0,4); //sender id */
+    /* String meters = String(data).substring(5); //distance */
+    //Did we receive data, and is it reasonable?
+  /*   if (sizeof(data) > 0 and meters.toFloat() < 100){ */
+  /*     //What car are we? */
+  /*     //Serial.println("here"); */
+  /*     for (int a = 0;a < num_ids; a++) { */
+  /*       if (my_id == car_ids[a]) { */
+  /*         range_msg.header.frame_id = car_num[a]; */
+  /*       } */
+  /*       //what car are you? */
+  /*       else if (your_id == car_ids[a]) { */
+  /*         range_msg.field_of_view = String(car_num[a][3]).toFloat(); */
+  /*       } */
+  /*     } */
+  /*     //Zero meters is a misread */
+  /*     if (meters.toFloat() != 0) { */
+  /*       range_msg.range = meters.toFloat(); */
+  /*       pub_range.publish(&range_msg); */
+  /*       meas.gps.latitude = meters.toFloat(); */
+  /*       pub_meas.publish(&meas); */
+  /*     } */
+  /*   } */
+  /* } */
+  }
+    nh.spinOnce();
+}
+
---
+++ sketchbook/pozyx_sender_range/pozyx_sender_range.ino
@@ -0,0 +1,127 @@
+#include <Pozyx.h>
+#include <Pozyx_definitions.h>
+#include <Wire.h>
+#include <ros.h>
+#include <sensor_msgs/NavSatFix.h>
+
+/*void getgps(const sensor_msgs::NavSatFix& gps_msg)
+  if(gps_msg.data > 1.0)
+    digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+}*/
+
+uint16_t source_id;                 // the network id of this device
+uint16_t chat_id = 0;             //Broadcast the message
+int status;
+
+uint8_t ranging_protocol = POZYX_RANGE_PROTOCOL_PRECISION; // ranging protocol of the Pozyx.
+/* const int num_cars = 2; //Amount of other cars */
+const int num_cars = 1; //Amount of other cars
+/* uint16_t car_ids[num_cars] = {0x6806,0x6827}; //Default is car0 sender so only range car1,car2 receivers */
+uint16_t car_ids[num_cars] = {0x6806}; //Default is car0 sender so only range car1,car2 receivers
+
+void setup_uwb()
+{
+    UWB_settings_t uwb_settings;
+    Pozyx.getUWBSettings(&uwb_settings);
+    uwb_settings.bitrate = 2;
+    uwb_settings.plen = 0x24;
+    Pozyx.setUWBSettings(&uwb_settings);
+}
+
+void setup(){
+  Serial.begin(57600);
+  // initialize Pozyx
+  if(Pozyx.begin() == POZYX_FAILURE){
+    Serial.println("ERROR: Unable to connect to POZYX shield");
+    Serial.println("Reset required");
+    delay(100);
+    abort();
+  }
+    /* delay(1000); */
+    //setup_uwb();
+  // read the network id of this device
+  /* Pozyx.setOperationMode(POZYX_ANCHOR_MODE); */
+  delay(1000);
+  Pozyx.regRead(POZYX_NETWORK_ID, (uint8_t*)&source_id, 2);
+  if (String(source_id,HEX) == "6867"){ //This is car1 sender
+    car_ids[0] = 0x6802; //so only range car0 receiver
+    /* car_ids[1] = 0x6827; //and car2 reciever */
+  }
+  else if (String(source_id,HEX) == "685b"){ //This is car2 sender
+    car_ids[0] = 0x6802; //so only range car0
+    car_ids[1] = 0x6806; //and car1
+  }
+}
+
+void discover()
+{
+    Pozyx.clearDevices();
+    int status = Pozyx.doDiscovery(POZYX_DISCOVERY_ANCHORS_ONLY);
+    if (status == POZYX_SUCCESS)
+    {
+        uint8_t n_devs = 0;
+        status = 0;
+        status = Pozyx.getDeviceListSize(&n_devs);
+        if (n_devs > 0)
+        {
+            uint16_t devices[n_devs];
+            Pozyx.getDeviceIds(devices, n_devs);
+            for (int i = 0; i < n_devs; i++)
+            {
+                Serial.println(String(devices[i], HEX));
+            }
+        }
+    }
+    else
+    {
+    }
+}
+
+void loop()
+{
+    discover();
+    send_message();
+}
+
+typedef struct dq_range_t {
+    uint16_t id;
+    float dist;
+} dq_range;
+
+typedef struct dq_header_t {
+    uint8_t sensor_type;
+    uint16_t num_data;
+} dq_header;
+
+void send_message() {
+    int car_data_size = sizeof(dq_range);
+    size_t total_car_data = sizeof(dq_header) + num_cars * car_data_size;
+    uint8_t buffer[total_car_data];
+    dq_header header = {0, num_cars};
+
+    // writes message header
+    memcpy(&buffer[0], &header, sizeof(dq_header));
+
+    for (int i = 0; i < num_cars; i++) {
+        if (String(source_id,HEX) != String(car_ids[i],HEX)) {
+            device_range_t range;
+            status = 0;
+            while (status != POZYX_SUCCESS) {
+                status = Pozyx.doRanging(car_ids[i], &range);
+                if (status == POZYX_SUCCESS) {
+                    float dist = range.distance / 1000.0;
+                    dq_range rng = {car_ids[i], dist};
+                    memcpy(buffer + i * car_data_size + sizeof(dq_header),
+                        &rng, sizeof(dq_range));
+                    break;
+                }
+            }
+        }
+    }
+    status = Pozyx.writeTXBufferData(buffer, total_car_data);
+    // broadcast the contents of the TX buffer
+    status = Pozyx.sendTXBufferData(chat_id);
+    delay(1);
+}
+
+
